8c16439ec2688fa899e8aae387a4f986743272b9
Zackary Goncalves <zackarygoncalves@gmail.com>
2026-02-21 20:03:03 -0600

    player object

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
 from constants import *
 from logger import log_state
+from cicleshape import *
+from player import *
 
 def main():
     pygame.init()
 
+    #FPS
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    Player(x, y)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,7 +22,9 @@ def main():
         for event in pygame.event.get():
             log_state()
             screen.fill("black")
+            player.draw(screen)
             pygame.display.flip()
+            dt = clock.tick(60) / 1000
             if event.type == pygame.QUIT:
                 return
         pass
